76a527e3aaea789452638fbefdfba4da3e751c63
AlaaHmaidat <alaahmidat2000@gmail.com>
2023-04-12 20:55:00 +0000

    initialize the code

---
+++ pythonic-garage-band/pythonic-garage-band.py
@@ -0,0 +1,24 @@
+class band():
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__():
+        pass
+
+
+
+class Musician():
+    pass
+
+def Guitarist():
+    pass
+
+def Drummer():
+    pass
+
+def Bassist():
+    pass
+
